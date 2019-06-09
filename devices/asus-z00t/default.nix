976111bbd5730c33f62b42f8996bcd1453cb54cb
Samuel Dionne-Riel <samuel@dionne-riel.com>
2019-06-09 13:23:39 -0400

    asus-z00t: wip

--- devices/asus-z00t/default.nix
+++ devices/asus-z00t/default.nix
@@ -1,11 +1,24 @@
 { config, lib, pkgs, ... }:
 
+let
+  device_info = (lib.importJSON ../postmarketOS-devices.json).asus-z00t;
+in
 {
   mobile.device.name = "asus-z00t";
-  mobile.device.info = (lib.importJSON ../postmarketOS-devices.json).asus-z00t // rec {
+  mobile.device.info = device_info // rec {
     # TODO : make kernel part of options.
     kernel = pkgs.callPackage ./kernel { kernelPatches = pkgs.defaultKernelPatches; };
     dtb = "${kernel}/dtbs/asus-z00t.img";
+
+    kernel_cmdline = lib.concatStringsSep " " [
+      "console=tty0"
+      "androidboot.hardware=qcom"
+      "ehci-hcd.park=3"
+      "androidboot.bootdevice=7824900.sdhci"
+      "lpm_levels.sleep_disabled=1"
+      "androidboot.selinux=permissive"
+    ];
+
   };
   mobile.hardware = {
     # This could also be pre-built option types?
