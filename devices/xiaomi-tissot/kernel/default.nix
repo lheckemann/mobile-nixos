1e855dca53b2f951a43f1c9c55436085a6f8628f
Linus Heckemann <git@sphalerite.org>
2019-10-28 15:33:38 +0100

    tissot: use linux 4.9 (not working)
    Builds fine, but:
    
    $ fastboot boot boot.img
    Downloading 'boot.img'    OKAY [  1.057s]
    booting                   FAILED (remote: 'dtb not found')

--- devices/xiaomi-tissot/kernel/default.nix
+++ devices/xiaomi-tissot/kernel/default.nix
@@ -6,19 +6,19 @@
 }:
 let inherit (buildPackages) dtc; in
 (mobile-nixos.kernel-builder-gcc6 {
-  version = "3.18.71";
+  version = "4.9.188";
   configfile = ./config;
   file = "Image.gz-dtb";
   hasDTB = true;
   src = fetchFromGitHub {
     owner = "lineageos";
     repo = "android_kernel_xiaomi_msm8953";
-    rev = "80cb3f607eb78280642c3b9b6e89f676e9c263bf";
-    sha256 = "13p326acpyqvlh5524bvy2qkgzgyhwxgy0smlwmcdl6y7yi04rg5";
+    rev = "337f9d5a8111b22f7a22cfaf3a3ab17370369e61";
+    sha256 = "0wq7glwi5rdysg8lpy80skyr04qzmdqv8zlc7sf0n9iazz4mavkw";
   };
   patches = [
-    ../../motorola-addison/kernel/99_framebuffer.patch
     ../../xiaomi-lavender/kernel/0003-arch-arm64-Add-config-option-to-fix-bootloader-cmdli.patch
+    ../../xiaomi-lavender/kernel/0001-mobile-nixos-Workaround-selected-processor-does-not-.patch
   ];
 
   postPatch = ''
